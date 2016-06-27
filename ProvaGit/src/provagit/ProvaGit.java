fc239c60ec633db4fdf289d196f8ae53f791511a
Antonio <Antonio@Antonio-PC>
2016-06-27 13:00:57 +0200

    Salvato

--- ProvaGit/src/provagit/ProvaGit.java
+++ ProvaGit/src/provagit/ProvaGit.java
@@ -15,7 +15,7 @@ public class ProvaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("A");
+        // TODO code application logic here
     }
     
 }
